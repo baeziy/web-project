c90d2ea59baaec22dad7a550b7865a9f8187bcac
Mustafa Kamal <mustfakmalik@gmail.com>
2023-02-13 01:10:04 +0500

    add ejs-mate and add .ejs files in products folder. Also added boilerplate in layouts folder

--- server.js
+++ server.js
@@ -13,24 +13,28 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
+//home page
 app.get('/', (req, res)=>{
     res.render('boilerplate', {who: 'home'})
 });
 
+//view all products page
 app.get('/products', async (req, res)=>{
      const products = await Product.find({});
-     res.render('allproducts', {products});
+     res.render('products/allproducts', {products: products, title: 'All Products'});
 });
 
+// new product form page
+app.get('products/new', async (res, req) =>{
+    
+})
 app.get('/product/:id', async (req, res) =>{
     const product = await Product.findById(req.params.id);
-    res.render('singleProduct', {product});
+    res.render('products/singleProduct', {product: product, title: product.title});
 
 });
 
-app.get('/new', async (res, req) =>{
-})
+
 
 
 
