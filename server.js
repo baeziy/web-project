85a8eb0e400bcda92b69b9285439bb154f525f84
Mustafa Kamal <mustfakmalik@gmail.com>
2023-02-13 02:59:28 +0500

    basic CRUD added + more .ejs files made

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const connectDB = require('./config/db');
 const engine = require('ejs-mate');
 const port = process.env.PORT || 3000;
 const Product = require('./models/productModel');
+const methodOverride = require('method-override');
 
 connectDB();
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 //home page
 app.get('/', (req, res)=>{
@@ -25,18 +29,41 @@ app.get('/products', async (req, res)=>{
 });
 
 // new product form page
-app.get('products/new', async (res, req) =>{
-    
+app.get('/products/new', (req, res) =>{
+    res.render('products/newProduct', {title: 'New Product'});
 })
-app.get('/product/:id', async (req, res) =>{
+
+// saving the data got from form to database
+app.post('/products', async (req, res)=>{
+    const product = await Product.create(req.body.product);
+    res.redirect(`/products/${product.id}`);
+
+})
+
+// view specific product
+app.get('/products/:id', async (req, res) =>{
     const product = await Product.findById(req.params.id);
     res.render('products/singleProduct', {product: product, title: product.title});
 
 });
 
+// edit product page
+app.get('/products/:id/edit', async (req, res) =>{
 
+    const product = await Product.findById(req.params.id);
+    res.render('products/productEdit', {product: product, title: 'Edit Product'});
+});
 
+app.put('/products/:id', async (req, res) =>{
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body.product);
+    res.redirect(`/products/${product.id}`);
+});
 
+app.delete('/products/:id/del', async (req, res)=>{
+    await Product.findByIdAndDelete(req.params.id);
+    res.redirect(`/products`);
+
+})
 
 
 app.listen(port, (req, res)=> console.log(`Listening at Port = ${port}`));
