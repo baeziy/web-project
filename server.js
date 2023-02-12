f65bb09618286d6b9878d107b32f56ae969f26ab
Mustafa Kamal <mustfakmalik@gmail.com>
2023-02-12 22:18:32 +0500

    add config file and app.set()

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv').config();
+const path = require('path');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 3000;
+
+connectDB();
+
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.send('All products');
+});
+
 
 
 app.listen(port, (req, res)=> console.log(`Listening at Port = ${port}`));
