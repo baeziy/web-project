c1d6174f24b70cd67ce5f4f49135a5efd8b448ca
Mustafa Kamal <mustfakmalik@gmail.com>
2023-02-14 12:49:26 +0500

    add review Model, connected with product Model, edited to add reviews in singleProduct.ejs

--- models/productModel.js
+++ models/productModel.js
@@ -22,7 +22,14 @@ const productSchema = new Schema({
     description: {
         type: String,
         required: [true, "Please enter a description"]
-    }
+    },
+
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        },
+    ]
 
 },{
     timestamps: true
